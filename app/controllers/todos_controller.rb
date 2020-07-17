7c104bbc7f1c5eb1325c1802244ee5cf0110fa30
Zietieflr <lrmcguire93@gmail.com>
2020-07-17 17:11:58 -0600

    Add create and delete routes. Tested.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,27 @@
 class TodosController < ApplicationController
 
   def index 
-    @todos = Todo.ApplicationController
+    @todos = Todo.all 
     
     render json: @todos
   end 
+
+  def create 
+    @todo = Todo.create(
+      title: params[:title],
+      content: params[:content],
+      urgent: params[:urgent],
+      done: params[:done],
+    )
+
+    render json: @todo, status: :created
+  end
+
+  def destroy 
+    @todo = Todo.find(params[:id])
+
+    @todo.destroy 
+    
+    render status: :no_content
+  end
 end

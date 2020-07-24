82e9b5f5256662261a72a3caa9d364cffb835e50
Zietieflr <lrmcguire93@gmail.com>
2020-07-24 15:22:19 -0600

    Add patch route. Add strong perams. Add before action for delete  and patch.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,27 +1,37 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:update, :destroy]
 
-  def index 
-    @todos = Todo.all 
-    
+  def index
+    @todos = Todo.all
+
     render json: @todos
-  end 
+  end
 
-  def create 
-    @todo = Todo.create(
-      title: params[:title],
-      content: params[:content],
-      urgent: params[:urgent],
-      done: params[:done],
-    )
+  def create
+    @todo = Todo.create(todo_params)
 
     render json: @todo, status: :created
   end
+  
+  def update
+    @todo.update(todo_params)
 
-  def destroy 
-    @todo = Todo.find(params[:id])
+    render json: @todo, status: :ok
+  end
 
-    @todo.destroy 
-    
+  def destroy
+    @todo.destroy
+
     render status: :no_content
+  end
+
+  private
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :content, :urgent, :done)
   end
 end
